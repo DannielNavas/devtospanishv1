a61c9c09e719a5e8bad4e076facda3ec7679ecb2
Danniel Navas <dannielnavas@gmail.com>
2023-10-17 21:35:06 -0500

    ✨ create header

---
+++ src/components/Header.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <header class="w-full h-24 text-white flex justify-between items-center px-6 bg-slate-800">
+        <h1 class="text-xl">Dev.to Comunidad en español</h1>
+        <nav class="flex space-x-6">
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+        </nav>
+    </header>
+</template>
