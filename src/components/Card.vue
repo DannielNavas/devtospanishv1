e299112f4ef8027e48387788599a01809383c6be
Danniel Navas <dannielnavas@gmail.com>
2023-11-13 21:24:02 -0500

    ✨ Add social media sharing icons and update card layout

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,6 +1,7 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import { toRefs } from 'vue';
+import { IconHeart, IconMessage, IconShare2 } from '@tabler/icons-vue';
+import { computed, defineProps, toRefs } from 'vue';
 
 
 const props = defineProps({
@@ -23,21 +24,91 @@ const props = defineProps({
         type: String,
         required: true,
         default: 'https://picsum.photos/200/300',
+    },
+    url: {
+        type: String,
+        required: true,
+        default: 'https://google.com',
+    },
+    author: {
+        type: String,
+        required: true,
+        default: 'Author',
+    },
+    reactions: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    comments: {
+        type: Number,
+        required: true,
+        default: 0,
+    },
+    readingTime: {
+        type: Number,
+        required: true,
+        default: 0,
     }
 });
 
-const { title, description, date, image } = toRefs(props);
+const { title, description, date, image, url, reactions, comments } = toRefs(props);
+
+const share = async (url: string, title: string, description: string) => {
+    event?.preventDefault();
+    const shareData = {
+        title,
+        text: description,
+        url,
+    };
+    try {
+        if (navigator.share && navigator.canShare(shareData)) {
+            await navigator.share(shareData);
+        } else {
+            console.log('Web Share API not supported.');
+        }
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const dateFormat = computed(() => {
+    const newDate = new Date(date.value);
+    return newDate.toLocaleString('es-ES', {
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric',
+    });
+});
+
 </script>
 <template>
-    <article class="w-80 h-96 border-slate-800 rounded-lg border-current bg-orange-400 p-6">
-        <img :src="image" alt="" class="w-full h-1/2 rounded-lg">
-        <!-- date -->
-        <div class="flex justify-between items-center mt-3">
-            <p class="text-xs">{{ date }}</p>
-            <p class="text-xs">author</p>
-        </div>
-        <h2 class="text-xl font-bold mt-4">{{ title }}</h2>
-        <p class="text-sm mt-2">{{ description }}</p>
-
-    </article>
+    <a :href="url" target="_blank" rel="noopener noreferrer">
+        <article class="lg:w-96 sm:w-72 h-max border-slate-800 rounded-lg border border-current p-4 dark:border-slate-50">
+            <p class="text-xs w-full">{{ author }}</p>
+            <img :src="image" :alt="title" class="w-full h-36 object-fill rounded-lg" loading="lazy">
+            <div class="flex justify-between items-center mt-3">
+                <p class="text-xs">{{ dateFormat }}</p>
+                <p class="text-xs">{{ readingTime }}
+                    <span>
+                        {{ readingTime === 1 ? 'minuto' : 'minutos' }} de lectura
+                    </span>
+                </p>
+            </div>
+            <h2 class="text-xl font-bold mt-4 h-20">{{ title }}</h2>
+            <!-- <p class="text-sm mt-2 h-16">{{ description }}</p> -->
+            <div class="flex justify-evenly mt-4">
+                <!-- icons -->
+                <div class="flex justify-center items-center gap-2">
+                    <IconHeart class="text-red-500" />
+                    <p class="m-0 text-lg">{{ reactions }}</p>
+                </div>
+                <div class="flex justify-center items-center gap-2">
+                    <IconMessage class="text-blue-500" />
+                    <p class="m-0 text-lg">{{ comments }}</p>
+                </div>
+                <IconShare2 class="text-green-500" @click="share(url, title, description)" />
+            </div>
+        </article>
+    </a>
 </template>
