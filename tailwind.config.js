f546649468feb750ff2e716b82b19257df3b7044
Danniel Navas <dannielnavas@gmail.com>
2023-11-13 21:23:24 -0500

    ✨ Add dark mode and custom variants to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
     extend: {}
   },
-  plugins: []
+  plugins: [],
+  variants: {
+    with: ['responsive', 'hover', 'focus', 'active'],
+    extend: {
+      fontFamily: {
+        Monserrat: ['Monserrat', 'sans-serif']
+      }
+    }
+  }
 }
