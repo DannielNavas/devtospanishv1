cf9f43be5af39b76acaf471cb14e50988050fb3b
Danniel Navas <dannielnavas@gmail.com>
2023-11-21 20:14:17 -0500

    ✨ Refactor allPosts computed property in HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,20 +10,9 @@ const posts = ref([]);
 
 
 const allPosts = computed(() => {
-  return posts.value.map((post: {
-    [x: string]: any; id: number, title: any; description: any; published_at: any; cover_image: any; social_image: string, url: string, canonical_url: string
-  }) => {
+  return posts.value.map((post: any) => {
     return {
-      id: post.id,
-      title: post.title,
-      description: post.description,
-      date: post.published_at,
-      image: post.cover_image ?? post.social_image,
-      url: post.url ?? post.canonical_url,
-      author: post.user.name,
-      reactions: post.public_reactions_count,
-      comments: post.comments_count,
-      readingTime: post.reading_time_minutes,
+      ...post,
     };
   });
 });
@@ -52,10 +41,11 @@ const onCancel = () => {
   <loading :active="isLoading" :can-cancel="true" :on-cancel="onCancel" :is-full-page="fullPage" />
   <Suspense>
     <main class="w-screen h-full flex flex-wrap gap-8 justify-center px-16 mt-6">
-      <Card suspense v-for="article in allPosts" :key="article.id" :title="article.title"
-        :description="article.description" :date="article.date" :image="article.image" :url="article.url"
-        :author="article.author" :reactions="article.reactions" :comments="article.comments"
-        :readingTime="article.readingTime" />
+      <Card suspense v-for="article in allPosts" :key="article.id" :article="article">
+        <template #default>
+          {{ article ? article.title : 'Cargando...' }}
+        </template>
+      </Card>
     </main>
   </Suspense>
 </template>
