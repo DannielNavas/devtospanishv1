921b3fbd1a8945101de17095b04bea80b2eda8b3
Danniel Navas <dannielnavas@gmail.com>
2023-11-27 21:32:22 -0500

    ✨ Update AboutView.vue with Spanish content

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,64 @@
 <template>
-  <main>
-    <h1>About</h1>
-  </main>
+  <div class="container mx-auto px-8 mt-16">
+    <h1>Acerca de</h1>
+
+    <p>DevToSpanish es un proyecto creado por Daniel Navas, un desarrollador web colombiano que busca promover el
+      contenido
+      en español en la plataforma Dev.to.</p>
+
+    <p>El proyecto tiene como premisas principales:</p>
+
+    <ul>
+      <li>Nunca monetizar: el contenido es de los autores, y el proyecto no busca obtener ningún beneficio económico de
+        él.
+      </li>
+      <li>Hacer visible a cada autor: el proyecto intenta que los autores de contenido en español reciban la mayor
+        visibilidad y apoyo posible.</li>
+      <li>El código sea abierto: el código del proyecto es de código abierto, para que cualquiera pueda contribuir a su
+        desarrollo.</li>
+      <li>Ser lo más sencillo posible: el proyecto debe ser fácil de iniciar y utilizar, para que cualquier persona pueda
+        aprovecharlo.</li>
+      <li>No quitar interacciones a Dev.to: el proyecto es solo una presentación con los artículos que redirigen a Dev.to,
+        para no quitarle interacciones a la plataforma original.</li>
+    </ul>
+
+    <p>Para aparecer en DevToSpanish, los autores deben agregar los tags "español", "spanish" o "espanol" a sus artículos.
+      Un API consulta estos tags y los presenta en DevToSpanish.</p>
+
+    <p>El proyecto está abierto a comentarios y mejoras. Daniel Navas espera que DevToSpanish ayude a la comunidad de
+      desarrolladores en español a crecer y prosperar.</p>
+
+    <h2>Historia</h2>
+
+    <p>El proyecto nació en noviembre de 2022, cuando Daniel Navas leyó un post en Dev.to en el que se preguntaba si
+      existía
+      más contenido en español en la plataforma. Daniel Navas, que es un desarrollador web colombiano, se sorprendió al
+      ver
+      que había mucho contenido en español de calidad en Dev.to.</p>
+
+    <p>Daniel Navas decidió crear un proyecto para promover este contenido. El proyecto debía cumplir con las premisas
+      mencionadas anteriormente, para que fuera respetuoso con los autores y la comunidad de Dev.to.</p>
+
+    <p>El proyecto se lanzó en diciembre de 2022, y rápidamente se convirtió en un recurso popular para los
+      desarrolladores
+      en español. El proyecto ha recibido comentarios positivos de la comunidad, y Daniel Navas espera que continúe
+      creciendo y ayudando a los desarrolladores en español.</p>
+
+
+    <h2>Futuro</h2>
+
+    <p>Daniel Navas tiene planes de seguir desarrollando el proyecto en el futuro. Algunas de las ideas que tiene
+      incluyen:
+    </p>
+
+    <ul>
+      <li>Añadir más funcionalidades, como la posibilidad de filtrar los artículos por categoría o autor.</li>
+      <li>Expandir el proyecto a otras plataformas de desarrollo.</li>
+    </ul>
+
+    <p>Daniel Navas está entusiasmado con el futuro de DevToSpanish, y cree que el proyecto tiene el potencial de
+      convertirse en un recurso indispensable para los desarrolladores en español.</p>
+  </div>
 </template>
 
 <style>
