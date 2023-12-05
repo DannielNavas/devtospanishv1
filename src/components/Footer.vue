da4528df77425e314b628108e44ad1611b793269
Danniel Navas <dannielnavas@gmail.com>
2023-12-04 22:09:27 -0500

    ✨ Update developer name in Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,7 +6,10 @@
     <div class=" bg-gray-900">
         <div class="max-w-2xl mx-auto text-white py-10">
             <div class="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
-                <p class="order-2 md:order-1 mt-8 md:mt-0"> Desarrollado por Danniel Navas para la comunidad. </p>
+                <p class="order-2 md:order-1 mt-8 md:mt-0"> Desarrollado por <a href="https://danniel.dev"
+                        class="font-bold decoration-white cursor-pointer underline" target="_blank">Danniel Navas</a> para
+                    la
+                    comunidad. </p>
                 <div class="order-1 md:order-2">
                     <!-- <span class="px-2">About us</span>
                     <span class="px-2 border-l">Contact us</span> -->
