51958d22ab68c0e4d570fc3923066927068dd8c2
Danniel Navas <dannielnavas@gmail.com>
2023-12-04 22:00:17 -0500

    ✨ Update Footer.vue with privacy policy link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,9 +8,13 @@
             <div class="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
                 <p class="order-2 md:order-1 mt-8 md:mt-0"> Desarrollado por Danniel Navas para la comunidad. </p>
                 <div class="order-1 md:order-2">
-                    <span class="px-2">About us</span>
-                    <span class="px-2 border-l">Contact us</span>
-                    <span class="px-2 border-l">Privacy Policy</span>
+                    <!-- <span class="px-2">About us</span>
+                    <span class="px-2 border-l">Contact us</span> -->
+                    <span class="px-2 border-l">
+                        <a href="https://dev.to/dannieldev/devto-en-espanol-4mgb" target="_blank" rel="noopener noreferrer">
+                            Privacy Policy
+                        </a>
+                    </span>
                 </div>
             </div>
         </div>
