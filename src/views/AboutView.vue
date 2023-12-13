1fb739df78b5250c931069d40837b8290b1181b6
Danniel Navas <dannielnavas@gmail.com>
2023-12-12 19:00:06 -0500

    ✨ Update AboutView.vue with styling and headings

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,24 +1,32 @@
 <template>
   <div class="container mx-auto px-8 mt-16">
-    <h1>Acerca de</h1>
+    <h1 class="text-3xl font-bold mb-8">Acerca de</h1>
 
-    <p>DevToSpanish es un proyecto creado por Daniel Navas, un desarrollador web colombiano que busca promover el
+    <p>DevToSpanish es un proyecto creado por Daniel Navas, un desarrollador web colombiano que busca
+      promover el
       contenido
       en español en la plataforma Dev.to.</p>
 
     <p>El proyecto tiene como premisas principales:</p>
 
-    <ul>
-      <li>Nunca monetizar: el contenido es de los autores, y el proyecto no busca obtener ningún beneficio económico de
+    <ul class="list-disc ml-8 mt-8 mb-8">
+      <li><span class="font-bold">Nunca monetizar:</span> el contenido es de los autores, y el proyecto no busca obtener
+        ningún beneficio económico de
         él.
       </li>
-      <li>Hacer visible a cada autor: el proyecto intenta que los autores de contenido en español reciban la mayor
-        visibilidad y apoyo posible.</li>
-      <li>El código sea abierto: el código del proyecto es de código abierto, para que cualquiera pueda contribuir a su
+      <li>
+        <span class="font-bold">Hacer visible a cada autor:</span> el proyecto intenta que los autores de contenido en
+        español reciban la mayor
+        visibilidad y apoyo posible.
+      </li>
+      <li><span class="font-bold">El código sea abierto:</span> el código del proyecto es de código abierto, para que
+        cualquiera pueda contribuir a su
         desarrollo.</li>
-      <li>Ser lo más sencillo posible: el proyecto debe ser fácil de iniciar y utilizar, para que cualquier persona pueda
+      <li><span class="font-bold">Ser lo más sencillo posible:</span> el proyecto debe ser fácil de iniciar y utilizar,
+        para que cualquier persona pueda
         aprovecharlo.</li>
-      <li>No quitar interacciones a Dev.to: el proyecto es solo una presentación con los artículos que redirigen a Dev.to,
+      <li><span class="font-bold">No quitar interacciones a Dev.to:</span> el proyecto es solo una presentación con los
+        artículos que redirigen a Dev.to,
         para no quitarle interacciones a la plataforma original.</li>
     </ul>
 
@@ -28,7 +36,7 @@
     <p>El proyecto está abierto a comentarios y mejoras. Daniel Navas espera que DevToSpanish ayude a la comunidad de
       desarrolladores en español a crecer y prosperar.</p>
 
-    <h2>Historia</h2>
+    <h2 class="text-2xl mt-8 mb-8">Historia</h2>
 
     <p>El proyecto nació en noviembre de 2022, cuando Daniel Navas leyó un post en Dev.to en el que se preguntaba si
       existía
@@ -45,13 +53,13 @@
       creciendo y ayudando a los desarrolladores en español.</p>
 
 
-    <h2>Futuro</h2>
+    <h2 class="text-2xl mt-8 mb-8">Futuro</h2>
 
     <p>Daniel Navas tiene planes de seguir desarrollando el proyecto en el futuro. Algunas de las ideas que tiene
       incluyen:
     </p>
 
-    <ul>
+    <ul class="ml-8 mb-8 mt-8 list-disc">
       <li>Añadir más funcionalidades, como la posibilidad de filtrar los artículos por categoría o autor.</li>
       <li>Expandir el proyecto a otras plataformas de desarrollo.</li>
     </ul>
